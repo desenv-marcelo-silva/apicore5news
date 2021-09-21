75132cdbe8684036f39570cd2a999e4f3d640702
[BOPE] Marcelo Silva <marcelo.lopes@meta.com.br>
2021-09-21 15:50:28 -0300

    chore: change image and vars to mongo/docker

--- scripts/docker-mongo.sh
+++ scripts/docker-mongo.sh
@@ -1,1 +1,5 @@
-docker run -d --rm --name mongo -p 27117:27017 -v mongodbdata:/data/db mongo
+docker run -d --rm --name mongo -p 27117:27017 \
+-v mongodbdata:/data/db \
+-e MONGODB_ROOT_USER=mongoadmin \
+-e MONGODB_ROOT_PASSWORD=Pass#word1 \
+bitnami/mongodb
