e10a10f97e22b8efecb3d8067133e89ce9ab8a22
[BOPE] Marcelo Silva <marcelo.lopes@meta.com.br>
2021-09-21 15:55:50 -0300

    feat: add simple health check

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,8 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +69,7 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
